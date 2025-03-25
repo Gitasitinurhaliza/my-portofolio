c73eb2a06fba8c86b2cf6e8cfdf3c17295be9ff8
Gitasitinurhaliza <ghitaaahlz@gmail.com>
2025-03-25 22:31:04 +0700

    upadate js

--- js/javascript.js
+++ js/javascript.js
@@ -1,17 +1,14 @@
-// Load EmailJS
 document.addEventListener("DOMContentLoaded", function() {
-    emailjs.init("emjBkTdHxxO0u5qqt"); // Ganti dengan User ID dari EmailJS
+    emailjs.init("emjBkTdHxxO0u5qqt"); 
 });
 
 document.querySelector(".contact-form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Mencegah halaman refresh
+    event.preventDefault(); 
 
-    // Ambil data dari form
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     let message = document.getElementById("message").value;
 
-    // Kirim data ke EmailJS
     emailjs.send("service_0cqs18o", "template_5g789gj", {
         from_name: name,
         from_email: email,
